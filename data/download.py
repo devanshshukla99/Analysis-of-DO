0687b58be3ded00f01f24d9182989ad8cd6b94d9
devanshshukla99 <devanshshukla99@outlook.com>
2022-04-10 18:53:52 +0530

    down ref

--- data/download.py
+++ data/download.py
@@ -1,16 +1,23 @@
 import os
 import datetime
 import subprocess
+import numpy as np
 
 API_KEY = "6a38abf402455bafad551e9c8063a6136e8438a2"
 url = "https://oceandata.sci.gsfc.nasa.gov/ob/getfile/"
 
-for i in range(10, 15):
-    _date = datetime.datetime(year=2018, month=1, day=i)
-    date = _date.strftime("%Y%m%d")
-    sst_aqua_pattern = f"AQUA_MODIS.{date}.L3m.DAY.SST.sst.4km.nc"
-    print(sst_aqua_pattern)
+_down_range = np.array(list(range(16, 30)))
+_down_range = _down_range.reshape(-1, 2)
 
-    p = os.popen(f"wget -O {sst_aqua_pattern} {url}{sst_aqua_pattern}?appkey={API_KEY}")
+for k in _down_range:
+    for j in k:
 
-p.wait()
+        _date = datetime.datetime(year=2018, month=1, day=j)
+        date = _date.strftime("%Y%m%d")
+        sst_aqua_pattern = f"AQUA_MODIS.{date}.L3m.DAY.SST.sst.4km.nc"
+        print(sst_aqua_pattern)
+        p = os.popen(f"wget -O {sst_aqua_pattern} {url}{sst_aqua_pattern}?appkey={API_KEY}")
+    print("\n")
+
+
+# p.wait()
